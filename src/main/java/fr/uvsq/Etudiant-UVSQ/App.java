71f050f7e15f7c6bba389f294bc5e7182ca88237
Hakim-OUED <ahakim.ouedraogo@gmail.com>
2019-11-11 16:21:59 +0100

    commit initial

---
+++ src/main/java/fr/uvsq/Etudiant-UVSQ/App.java
@@ -0,0 +1,13 @@
+package fr.uvsq.Etudiant-UVSQ;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
